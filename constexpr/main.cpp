366aca607cd30ed0a92f73c249ded4e0b08dfd19
Mobai Zhang <mzhang@appnexus.com>
2018-12-12 20:46:07 +0000

    checkpoint

--- constexpr/main.cpp
+++ constexpr/main.cpp
@@ -1,13 +1,55 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int f(void);
 //constexpr int f(void);
 
-int g(int x);
-int g(constexpr int x);
+class foo {
+public:
+	constexpr foo() noexcept = default;
+	constexpr foo(int xx) noexcept : x{xx} {}
+
+#if 0
+	foo(foo &&f) noexcept: x{f.x} {
+		cout << "move" << endl;
+	}
+
+	foo(const foo &f) : x{f.x} {
+		cout << "copy" << endl;
+	}
+#endif
+
+	foo(foo &&f) noexcept = default;
+
+
+	int x{0};
+};
+
+#if 0
+namespace std {
+
+template<>
+struct is_trivially_move_constructible<foo> : public integral_constant<bool, true> {
+};
+
+}
+
+#endif
 
 int main (int argc, char **argv) {
+	foo f;
+	constexpr foo ff(2);
+	int xx = 10;
+	foo fff(xx);
+
+	cout << "is_trivially_move_constructible<foo> = " << std::boolalpha << is_trivially_move_constructible<foo>::value << endl;
+
+	vector<foo> v;
+	for (auto i = 0; i != 3; ++i) {
+		cout << "capacity = " << v.capacity() << endl;
+		v.push_back(foo(0));
+	}
 	return 0;
 }
